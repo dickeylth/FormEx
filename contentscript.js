adb6e7082031a750351041d68802001995d378c5
弘树 <tiehang.lth@taobao.com>
2013-08-20 00:39:44 +0800

    完成表单快照创建

--- contentscript.js
+++ contentscript.js
@@ -11,20 +11,20 @@ chrome.runtime.sendMessage({}, function(response) {});
 
 // chrome优化的localStorage
 var storage = chrome.localStorage;
-console.log(storage);
+
+//  href, get url without query
+var href = window.location.href.split('?')[0];
 
 /**
  * 汇总api
  * @type {{snapshot: Function, recovery: Function, autofill: Function}}
  */
 var apis = {
-
     /**
      * 公用过滤输入节点的方法
      * @returns {{}}
      */
-    filterInput: function(){
-
+    filterNodes: function(){
         var validTags = ['input', 'textarea', 'select'];
         var allInputs = $(validTags.join(',')).filter(':visible');
         var excludeInputs = ['button', 'file', 'hidden', 'image', 'reset', 'submit'];
@@ -47,12 +47,11 @@ var apis = {
 
             if(tagName == 'input' && inpType && ($.inArray(inpType, excludeInputs) == -1)){
 
-
-                var checkArr = filtedInputs[tagName][inpType];
+                var typeObj = filtedInputs[tagName][inpType];
                 var key = (id && ("id=" + id)) || (name && ("name=" + name));
-                if(!checkArr){
-                    checkArr = {};
-                    checkArr[key] = {};
+                if(!typeObj){
+                    typeObj = {};
+                    typeObj[key] = {};
                 }
 
                 if(inpType == 'checkbox'){
@@ -60,16 +59,16 @@ var apis = {
                     // 对于checkbox，收集所有选中的value
                     if($item.prop('checked')){
 
-                        if($.isEmptyObject(checkArr[key])){
+                        if($.isEmptyObject(typeObj[key])){
                             // 还没有该类型的键值对
-                            var val = id ? $item.val() : [value];
-                            checkArr[key] = val;
+                            var val = id ? $item : [$item];
+                            typeObj[key] = $item;
                         }else{
                             // 已有此键值对数组，直接放入
                             if(id){
-                                checkArr[key] = $item.val();
+                                typeObj[key] = $item;
                             }else{
-                                checkArr[key].push(value);
+                                typeObj[key].push($item);
                             }
 
                         }
@@ -80,25 +79,25 @@ var apis = {
 
                     // 对于radio，只保留第一个被选中的值即可
                     if($item.prop('checked')){
-                        checkArr[key] = value;
+                        typeObj[key] = $item;
                     }
 
                 }else{
 
                     // 对于普通input，根据type及id存放到对应的映射数组中
                     if($.trim($item.val()) != ''){
-                        checkArr[key] = value;
+                        typeObj[key] = $item;
                     }
                 }
                 // 非引用，需赋回值
-                filtedInputs[tagName][inpType] = checkArr;
+                filtedInputs[tagName][inpType] = typeObj;
             }else{
                 // 对于select和textarea，优先按id保存值
 
                 if(id){
-                    filtedInputs[tagName]["id=" + id] = value;
+                    filtedInputs[tagName]["id=" + id] = $item;
                 }else if(name){
-                    filtedInputs[tagName]["name=" + name] = value;
+                    filtedInputs[tagName]["name=" + name] = $item;
                 }
             }
 
@@ -107,17 +106,42 @@ var apis = {
     },
 
     /**
+     * recusively transform filtedNodes into its value
+     * @param filtedNodes
+     * @returns {*}
+     */
+    transNodeToVal: function(filtedNodes){
+
+        var self = arguments.callee;
+
+        $.each(filtedNodes, function(idx, item){
+            if(!(item instanceof $)){
+                self.call(this, item);
+            }else{
+                filtedNodes[idx] = $.trim(item.val());
+            }
+        });
+
+        return filtedNodes;
+    },
+
+    /**
      * 创建快照
      */
     snapshot: function(){
-        var filtedInputs = apis.filterInput();
-        console.log(filtedInputs);
+
+        var filtedNodes = apis.filterNodes();
+        var filtedValues = apis.transNodeToVal(filtedNodes);
+        console.log(filtedValues);
+
+        //localStorage[href] = filtedInputs;
     },
 
     /**
      * 数据恢复
      */
     recovery: function(){
+        var storedData = localStorage[href];
 
     },
 
@@ -127,12 +151,8 @@ var apis = {
     autofill: function(){
 
     }
-}
+};
 
-$(document).ready(function(){
-    var inputs = $('input');
-    console.log(inputs);
-});
 
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
